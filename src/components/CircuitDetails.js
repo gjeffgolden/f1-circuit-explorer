7b935cb8e9fc597dea863c5e4676dcbb8e0b6794
gjeffgolden <gjgolden08@gmail.com>
2021-04-29 13:15:38 -0600

    Responsive styling.

--- src/components/CircuitDetails.js
+++ src/components/CircuitDetails.js
@@ -4,11 +4,10 @@ export default function CircuitDetails({ selectedCircuit }) {
     return (
         <div id="circuit-details">
             <h3>Details</h3>
-            <h5>Race: {selectedCircuit.competition.name}</h5>
+            <h5>{selectedCircuit.competition.location.city}, {selectedCircuit.competition.location.country}</h5>
+            <h5>{selectedCircuit.competition.name}</h5>
             <h5>Capacity: {selectedCircuit.capacity}</h5>
-            <h5>Opened: {selectedCircuit.opened}</h5>
             <h5>Length: {selectedCircuit.length}</h5>
-            <h5>Location: {selectedCircuit.competition.location.city}, {selectedCircuit.competition.location.country}</h5>
         </div>
     )
 }
